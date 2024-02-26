7af4aebf67529905d552063f4d2c48fee2035abc
Prachi Mudholkar <Prachi.Mudholkar@rishabhsoft.com>
2024-02-26 18:24:13 +0530

    Updated themeing and features

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-todo-list
 A simple React POC - Creating a To-Do list
+
+Icons- Add, Delete and Tick
+https://icons8.com/icon/Z2KRaTPsIv6p/plus
+https://icons8.com/icon/PN84GwwZXtfH/delete
+https://icons8.com/icon/EmPTDMRlslbb/checkmark
+https://icons8.com/icon/60010/thumbs-up
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from './tick.png';
+import logo from './tick.svg';
 import logo_purple from './tick-purple.png';
 import logo_zebra from './zebra-tick.gif';
 import logo_spider from './tick-spider.png';
@@ -22,6 +22,8 @@ function App() {
         const [addInputClass, setaddInputClass] = useState(false);
         const [taskdone, setTaskdone] = useState(false);
         const [selectedTheme, setSelectedTheme] = useState('veges');
+        const [hiddenButton, hiddenButtonShow] = useState(false);
+        const [hideTask, setHideTask] = useState(false);
 
         // Adds one input to create a todo list 
         const AddToDo = () => {
@@ -49,6 +51,7 @@ function App() {
             }
             setTodo([]);
             setShowInitialText(true);
+            hiddenButtonShow(false);
         };
 
         // Function to handle input 
@@ -63,13 +66,29 @@ function App() {
             const newTodos = [...todo];
             newTodos[index].completed = !newTodos[index].completed;
             if (newTodos[index].completed === true) {
+                hiddenButtonShow(true);
+
                 setTaskdone(true);
                 setTimeout(() => {
                     setTaskdone(false);
-                }, 1000)
+                }, 3000)
             }
             setTodo(newTodos);
         };
+
+        const hideCompletedTasks = () => {
+            const newTodos = [...todo];
+            
+            newTodos.forEach((todo, index) => {
+                if (todo.text.length > 0 && todo.completed) {
+                    setHideTask(true);
+                }
+            });
+        };
+
+        const showCompletedTasks = () => {
+            setHideTask(false);
+        }
 
         return (
             <div className={`App ${selectedTheme === 'purple' ? 'purple-themeing' : selectedTheme === 'veges' ? 'veges-themeing' : selectedTheme === 'zebra' ? 'zebra-themeing' : selectedTheme === 'spider' ? 'spider-themeing' : ''}`}>
@@ -92,101 +111,105 @@ function App() {
                         <span className='spider-theme'></span>
                     </div>
                 </div>
-                {/* Renders sparkling effect on completion of task */}
-                <div className={`sparkle-not ${taskdone ? 'tada' : ''}`}>
-                    {/* Renders app's title and logo */}
-                    <header className='todo-header'>
-                        <div className='App-logo'>
-                            {selectedTheme === 'veges' ? (
-                                <img src={logo} className='todo-logo' alt='logo of todo list'></img>
-                            ) : selectedTheme === 'purple' ? (
-                                <img src={logo_purple} className='todo-logo-purple' alt='logo of todo list'></img>
-                            ) : selectedTheme === 'zebra' ? (
-                                <img src={logo_zebra} className='todo-logo-zebra' alt='logo of todo list'></img>
-                            ) : selectedTheme === 'spider' ? (
-                                <img src={logo_spider} className='todo-logo-spider' alt='logo of todo list'></img>
-                            ) : ''
-                            }
-                        </div>
-                        <p className={`todo-title ${selectedTheme === 'purple' ? 'purple-title' : selectedTheme === 'veges' ? 'veges-title' : selectedTheme === 'zebra' ? 'zebra-title' : selectedTheme === 'spider' ? 'spider-title' : ''}`}>Let's do it</p>
-                        <p className={`todo-subtitle ${selectedTheme === 'purple' ? 'purple-subtitle' : selectedTheme === 'veges' ? 'veges-subtitle' : selectedTheme === 'zebra' ? 'zebra-subtitle' : selectedTheme === 'spider' ? 'spider-subtitle' : ''}`}>Get your tasks done, the right way !</p>
-                    </header>
 
-                    {/* Renders the todo list */}
-                    <div className={`todo-body ${selectedTheme === 'purple' ? 'purple-body' : selectedTheme === 'veges' ? 'veges-body' : selectedTheme === 'zebra' ? 'zebra-body' : selectedTheme === 'spider' ? 'spider-body' : ''}`}>
-                        {showInitialText &&
-                            <p className={`initial-todo-text ${selectedTheme === 'purple' ? 'purple-todo-text' : selectedTheme === 'veges' ? 'veges-todo-text' : selectedTheme === 'zebra' ? 'zebra-todo-text' : selectedTheme === 'spider' ? 'spider-todo-text' : ''}`}>
-                                Enlist your tasks here!
-                                <span className={`tooltiptext ${selectedTheme === 'purple' ? 'purple-tooltiptext' : selectedTheme === 'veges' ? 'veges-tooltiptext' : selectedTheme === 'zebra' ? 'zebra-tooltiptext' : selectedTheme === 'spider' ? 'spider-tooltiptext' : ''}`}>Please click the plus button</span>
-                            </p>
+                {/* Renders app's title and logo */}
+                <header className='todo-header'>
+                    <div className={`App-logo ${taskdone ? 'logo-hide' : ''}`}>
+                        {selectedTheme === 'veges' ? (
+                            <img src={logo} className='todo-logo' alt='logo of todo list'></img>
+                        ) : selectedTheme === 'purple' ? (
+                            <img src={logo_purple} className='todo-logo-purple' alt='logo of todo list'></img>
+                        ) : selectedTheme === 'zebra' ? (
+                            <img src={logo_zebra} className='todo-logo-zebra' alt='logo of todo list'></img>
+                        ) : selectedTheme === 'spider' ? (
+                            <img src={logo_spider} className='todo-logo-spider' alt='logo of todo list'></img>
+                        ) : ''
                         }
-                        <ul className='todo-list'>
-                            {todo.map((todo, index) => (
-                                <li key={index} className='todo-task'>
-                                    <input
-                                        type='checkbox'
-                                        checked={todo.completed}
-                                        className='todo-checkbox'
-                                        onChange={() => handleCheckboxChange(index)}>
-                                    </input>
-                                    <input
-                                        autoFocus
-                                        type='text'
-                                        placeholder='Enter your task'
-                                        className={`todo-input  ${selectedTheme === 'purple' ? 'purple-todo-input' : selectedTheme === 'veges' ? 'veges-todo-input' : selectedTheme === 'zebra' ? 'zebra-todo-input' : selectedTheme === 'spider' ? 'spider-todo-input' : ''} ${(!todo.text.length && addInputClass) ? 'input-error' : ''}`}
-                                        value={todo.text}
-                                        onChange={(event) => handleInputChange(index, event)}
-                                        required={true}
-                                        style={{ textDecoration: todo.completed ? 'line-through grey' : '', color: todo.completed ? 'grey' : '' }}>
-                                    </input>
-                                    <div style={{ display: 'none' }}
-                                        className={`${(!todo.text.length && addInputClass) ? 'input-empty' : ''} `}>
-                                        Please fill in this task
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
                     </div>
+                    <div className={`sparkle-not tada ${taskdone && (selectedTheme === 'purple' ? 'purple-tada' : selectedTheme === 'veges' ? 'veges-tada' : selectedTheme === 'zebra' ? 'zebra-tada' : selectedTheme === 'spider' ? 'spider-tada' : '')}`}></div>
+                    <p className={`todo-title ${selectedTheme === 'purple' ? 'purple-title' : selectedTheme === 'veges' ? 'veges-title' : selectedTheme === 'zebra' ? 'zebra-title' : selectedTheme === 'spider' ? 'spider-title' : ''}`}>Let's do it</p>
+                    <p className={`todo-subtitle ${selectedTheme === 'purple' ? 'purple-subtitle' : selectedTheme === 'veges' ? 'veges-subtitle' : selectedTheme === 'zebra' ? 'zebra-subtitle' : selectedTheme === 'spider' ? 'spider-subtitle' : ''}`}>Get your tasks done, the right way !</p>
+                </header>
 
-                    {/* Renders the action buttons for todo, add and delete */}
-                    <div className='todo-action-buttons'>
-                        {/* Button to add one task in the list */}
-                        <button className='todo-add-button' onClick={AddToDo}>
-                            {selectedTheme === 'veges' ? (
-                                <img src={plus} className='add-todo' alt='Button to add a task'></img>
-                            ) : selectedTheme === 'purple' ? (
-                                <img src={plus_purple} className='add-todo' alt='Button to add a task'></img>
-                            ) : selectedTheme === 'zebra' ? (
-                                <img src={plus_zebra} className='add-todo' alt='Button to add a task'></img>
-                            ) : selectedTheme === 'spider' ? (
-                                <img src={plus_spider} className='add-todo' alt='Button to add a task'></img>
-                            ) : ''
-                            }
-                        </button>
-                        {/* Button to delete the whole todo list */}
-                        <button className='todo-delete-button' onClick={DeleteToDo}>
-                            {selectedTheme === 'veges' ? (
-                                <img src={deleteicon} className='delete-todo' alt='Button to delete the whole list'></img>
+                {/* Renders the todo list */}
+                <div className={`todo-body ${selectedTheme === 'purple' ? 'purple-body' : selectedTheme === 'veges' ? 'veges-body' : selectedTheme === 'zebra' ? 'zebra-body' : selectedTheme === 'spider' ? 'spider-body' : ''}`}>
+                    {showInitialText &&
+                        <p className={`initial-todo-text ${selectedTheme === 'purple' ? 'purple-todo-text' : selectedTheme === 'veges' ? 'veges-todo-text' : selectedTheme === 'zebra' ? 'zebra-todo-text' : selectedTheme === 'spider' ? 'spider-todo-text' : ''}`}>
+                            Enlist your tasks here!
+                            <span className={`tooltiptext ${selectedTheme === 'purple' ? 'purple-tooltiptext' : selectedTheme === 'veges' ? 'veges-tooltiptext' : selectedTheme === 'zebra' ? 'zebra-tooltiptext' : selectedTheme === 'spider' ? 'spider-tooltiptext' : ''}`}>Please click the plus button</span>
+                        </p>
+                    }
+                    <ul className='todo-list'>
+                        {todo.map((todo, index) => (
+                            <li key={index} className={`todo-task`}>
+                                <input
+                                    type='checkbox'
+                                    checked={todo.completed && todo.text.trim() !== ''}
+                                    className={`todo-checkbox ${todo.completed && todo.text.length && hideTask ? 'todo-hide-task' : ''}`}
+                                    onChange={() => handleCheckboxChange(index)}>
+                                </input>
+                                <input
+                                    autoFocus
+                                    type='text'
+                                    placeholder='Enter your task'
+                                    className={`
+                                    todo-input  ${todo.completed && todo.text.length && hideTask ? 'todo-hide-task' : ''}
+                                    ${selectedTheme === 'purple' ? 'purple-todo-input' : selectedTheme === 'veges' ? 'veges-todo-input' : selectedTheme === 'zebra' ? 'zebra-todo-input' : selectedTheme === 'spider' ? 'spider-todo-input' : ''} ${(!todo.text.length && addInputClass) ? 'input-error' : ''}   ${todo.completed && todo.text.length && (selectedTheme === 'purple' ? 'purple-highlight' : selectedTheme === 'veges' ? 'veges-highlight' : selectedTheme === 'zebra' ? 'zebra-highlight' : selectedTheme === 'spider' ? 'spider-highlight' : '')} `}
+                                    value={todo.text}
+                                    onChange={(event) => handleInputChange(index, event)}
+                                    required={true}
+                                    style={{ textDecoration: (todo.completed && todo.text.length) ? 'line-through grey' : '', color: todo.completed ? 'grey' : '' }}>
+                                </input>
+                                <div style={{ display: 'none' }}
+                                    className={`${(!todo.text.length && addInputClass) ? 'input-empty' : ''} `}>
+                                    Please fill in this task
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
-                            ) : selectedTheme === 'purple' ? (
-                                <img src={delete_purple} className='delete-todo' alt='Button to delete the whole list'></img>
+                {/* Renders the action buttons for todo, add and delete */}
+                <div className='todo-action-buttons'>
+                    <button className={`todo-hidden ${hiddenButton ? 'todo-hidden-show' : ''}`} onClick={hideCompletedTasks} >Hide completed tasks</button>
+                    <button className={`todo-hidden ${hiddenButton ? 'todo-hidden-show' : ''}`} onClick={showCompletedTasks} >Show completed tasks</button>
 
-                            ) : selectedTheme === 'zebra' ? (
-                                <img src={delete_zebra} className='delete-todo' alt='Button to delete the whole list'></img>
+                    {/* Button to add one task in the list */}
+                    <button className='todo-add-button' onClick={AddToDo}>
+                        {selectedTheme === 'veges' ? (
+                            <img src={plus} className='add-todo' alt='Button to add a task'></img>
+                        ) : selectedTheme === 'purple' ? (
+                            <img src={plus_purple} className='add-todo' alt='Button to add a task'></img>
+                        ) : selectedTheme === 'zebra' ? (
+                            <img src={plus_zebra} className='add-todo' alt='Button to add a task'></img>
+                        ) : selectedTheme === 'spider' ? (
+                            <img src={plus_spider} className='add-todo' alt='Button to add a task'></img>
+                        ) : ''
+                        }
+                    </button>
+                    {/* Button to delete the whole todo list */}
+                    <button className='todo-delete-button' onClick={DeleteToDo}>
+                        {selectedTheme === 'veges' ? (
+                            <img src={deleteicon} className='delete-todo' alt='Button to delete the whole list'></img>
 
-                            ) : selectedTheme === 'spider' ? (
-                                <img src={delete_spider} className='delete-todo' alt='Button to delete the whole list'></img>
+                        ) : selectedTheme === 'purple' ? (
+                            <img src={delete_purple} className='delete-todo' alt='Button to delete the whole list'></img>
 
-                            ) : ''
-                            }
-                        </button>
+                        ) : selectedTheme === 'zebra' ? (
+                            <img src={delete_zebra} className='delete-todo' alt='Button to delete the whole list'></img>
 
-                    </div>
+                        ) : selectedTheme === 'spider' ? (
+                            <img src={delete_spider} className='delete-todo' alt='Button to delete the whole list'></img>
 
-
-
+                        ) : ''
+                        }
+                    </button>
 
                 </div>
+
+
+
+
             </div>
         );
     };
