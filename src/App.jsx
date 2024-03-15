54e9cb1ac671b7916eb76a481da59cadd98f0eae
Prachi Mudholkar <Prachi.Mudholkar@rishabhsoft.com>
2024-03-15 16:31:14 +0530

    Enabled local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './tick.svg';
 import logo_purple from './tick-purple.png';
 import logo_zebra from './zebra-tick.gif';
@@ -20,16 +20,59 @@ function App() {
         const [todo, setTodo] = useState([]);
         const [showInitialText, setShowInitialText] = useState(true);
         const [addInputClass, setaddInputClass] = useState(false);
-        const [taskdone, setTaskdone] = useState(false);
+        const [taskDone, setTaskdone] = useState(false);
+        const [tickDone, setTickdone] = useState(false);
         const [selectedTheme, setSelectedTheme] = useState('veges');
         const [hiddenButton, hiddenButtonShow] = useState(false);
         const [hideTask, setHideTask] = useState(false);
+
+        useEffect(() => {
+            const storedTodos = JSON.parse(localStorage.getItem('todo'));
+            const doneTodos = JSON.parse(localStorage.getItem('tickDone'));
+            const hideTodos = JSON.parse(localStorage.getItem('hiddenButton'));
+            const hiddenTask = JSON.parse(localStorage.getItem('hideTask'));
+
+            if (storedTodos) {
+                setTodo(storedTodos);
+            }
+
+            if (doneTodos) {
+                setTickdone(doneTodos);
+            }
+
+            if (hideTodos) {
+                hiddenButtonShow(hideTodos);
+            }
+
+            if (hiddenTask) {
+                setHideTask(hiddenTask);
+            }
+
+            if (storedTodos && storedTodos.length) {
+                hiddenButtonShow(true);
+                setShowInitialText(false);
+                localStorage.setItem('setShowInitialText', JSON.stringify(false));
+            }
+            else {
+                setShowInitialText(true);
+                localStorage.setItem('setShowInitialText', JSON.stringify(true));
+            }
+
+        }, []);
+
+        const handleKeyPress = (event) => {
+            if (event.key === 'Enter') {
+                event.preventDefault(); // Prevent default behavior of Enter key
+                AddToDo();
+            }
+        };
 
         // Adds one input to create a todo list 
         const AddToDo = () => {
             const currentTodos = [...todo];
             if (currentTodos.length > 0 && !currentTodos[currentTodos.length - 1].text) {
                 setaddInputClass(true);
+                console.log(currentTodos.length);
                 return false;
             }
             else {
@@ -52,6 +95,7 @@ function App() {
             setTodo([]);
             setShowInitialText(true);
             hiddenButtonShow(false);
+            localStorage.clear();
         };
 
         // Function to handle input 
@@ -59,6 +103,7 @@ function App() {
             const newTodos = [...todo];
             newTodos[index].text = event.target.value;
             setTodo(newTodos);
+            localStorage.setItem('todo', JSON.stringify(newTodos));
         };
 
         // Function to handle checkbox state changes
@@ -67,27 +112,36 @@ function App() {
             newTodos[index].completed = !newTodos[index].completed;
             if (newTodos[index].completed === true) {
                 hiddenButtonShow(true);
-
+                setTickdone(true);
+                localStorage.setItem('todo', JSON.stringify(newTodos));
+                localStorage.setItem('setTickdone', JSON.stringify(true));
                 setTaskdone(true);
                 setTimeout(() => {
                     setTaskdone(false);
                 }, 3000)
             }
+            else {
+                setTickdone(false);
+                localStorage.setItem('todo', JSON.stringify(newTodos));
+                localStorage.setItem('setTickdone', JSON.stringify(false));
+            }
+            localStorage.setItem('hiddenButtonShow', JSON.stringify(true));
             setTodo(newTodos);
         };
 
         const hideCompletedTasks = () => {
             const newTodos = [...todo];
-            
             newTodos.forEach((todo, index) => {
                 if (todo.text.length > 0 && todo.completed) {
                     setHideTask(true);
+                    localStorage.setItem('hideTask', JSON.stringify(true));  
                 }
             });
         };
 
         const showCompletedTasks = () => {
             setHideTask(false);
+            localStorage.setItem('hideTask', JSON.stringify(false));
         }
 
         return (
@@ -114,7 +168,7 @@ function App() {
 
                 {/* Renders app's title and logo */}
                 <header className='todo-header'>
-                    <div className={`App-logo ${taskdone ? 'logo-hide' : ''}`}>
+                    <div className={`App-logo ${taskDone ? 'logo-hide' : ''}`}>
                         {selectedTheme === 'veges' ? (
                             <img src={logo} className='todo-logo' alt='logo of todo list'></img>
                         ) : selectedTheme === 'purple' ? (
@@ -126,53 +180,15 @@ function App() {
                         ) : ''
                         }
                     </div>
-                    <div className={`sparkle-not tada ${taskdone && (selectedTheme === 'purple' ? 'purple-tada' : selectedTheme === 'veges' ? 'veges-tada' : selectedTheme === 'zebra' ? 'zebra-tada' : selectedTheme === 'spider' ? 'spider-tada' : '')}`}></div>
+                    <div className={`sparkle-not tada ${taskDone && (selectedTheme === 'purple' ? 'purple-tada' : selectedTheme === 'veges' ? 'veges-tada' : selectedTheme === 'zebra' ? 'zebra-tada' : selectedTheme === 'spider' ? 'spider-tada' : '')}`}></div>
                     <p className={`todo-title ${selectedTheme === 'purple' ? 'purple-title' : selectedTheme === 'veges' ? 'veges-title' : selectedTheme === 'zebra' ? 'zebra-title' : selectedTheme === 'spider' ? 'spider-title' : ''}`}>Let's do it</p>
                     <p className={`todo-subtitle ${selectedTheme === 'purple' ? 'purple-subtitle' : selectedTheme === 'veges' ? 'veges-subtitle' : selectedTheme === 'zebra' ? 'zebra-subtitle' : selectedTheme === 'spider' ? 'spider-subtitle' : ''}`}>Get your tasks done, the right way !</p>
                 </header>
 
-                {/* Renders the todo list */}
-                <div className={`todo-body ${selectedTheme === 'purple' ? 'purple-body' : selectedTheme === 'veges' ? 'veges-body' : selectedTheme === 'zebra' ? 'zebra-body' : selectedTheme === 'spider' ? 'spider-body' : ''}`}>
-                    {showInitialText &&
-                        <p className={`initial-todo-text ${selectedTheme === 'purple' ? 'purple-todo-text' : selectedTheme === 'veges' ? 'veges-todo-text' : selectedTheme === 'zebra' ? 'zebra-todo-text' : selectedTheme === 'spider' ? 'spider-todo-text' : ''}`}>
-                            Enlist your tasks here!
-                            <span className={`tooltiptext ${selectedTheme === 'purple' ? 'purple-tooltiptext' : selectedTheme === 'veges' ? 'veges-tooltiptext' : selectedTheme === 'zebra' ? 'zebra-tooltiptext' : selectedTheme === 'spider' ? 'spider-tooltiptext' : ''}`}>Please click the plus button</span>
-                        </p>
-                    }
-                    <ul className='todo-list'>
-                        {todo.map((todo, index) => (
-                            <li key={index} className={`todo-task`}>
-                                <input
-                                    type='checkbox'
-                                    checked={todo.completed && todo.text.trim() !== ''}
-                                    className={`todo-checkbox ${todo.completed && todo.text.length && hideTask ? 'todo-hide-task' : ''}`}
-                                    onChange={() => handleCheckboxChange(index)}>
-                                </input>
-                                <input
-                                    autoFocus
-                                    type='text'
-                                    placeholder='Enter your task'
-                                    className={`
-                                    todo-input  ${todo.completed && todo.text.length && hideTask ? 'todo-hide-task' : ''}
-                                    ${selectedTheme === 'purple' ? 'purple-todo-input' : selectedTheme === 'veges' ? 'veges-todo-input' : selectedTheme === 'zebra' ? 'zebra-todo-input' : selectedTheme === 'spider' ? 'spider-todo-input' : ''} ${(!todo.text.length && addInputClass) ? 'input-error' : ''}   ${todo.completed && todo.text.length && (selectedTheme === 'purple' ? 'purple-highlight' : selectedTheme === 'veges' ? 'veges-highlight' : selectedTheme === 'zebra' ? 'zebra-highlight' : selectedTheme === 'spider' ? 'spider-highlight' : '')} `}
-                                    value={todo.text}
-                                    onChange={(event) => handleInputChange(index, event)}
-                                    required={true}
-                                    style={{ textDecoration: (todo.completed && todo.text.length) ? 'line-through grey' : '', color: todo.completed ? 'grey' : '' }}>
-                                </input>
-                                <div style={{ display: 'none' }}
-                                    className={`${(!todo.text.length && addInputClass) ? 'input-empty' : ''} `}>
-                                    Please fill in this task
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-
-                {/* Renders the action buttons for todo, add and delete */}
-                <div className='todo-action-buttons'>
-                    <button className={`todo-hidden ${hiddenButton ? 'todo-hidden-show' : ''}`} onClick={hideCompletedTasks} >Hide completed tasks</button>
-                    <button className={`todo-hidden ${hiddenButton ? 'todo-hidden-show' : ''}`} onClick={showCompletedTasks} >Show completed tasks</button>
+                 {/* Renders the action buttons for todo, add and delete */}
+                 <div className='todo-action-buttons'>
+                    <button className={`todo-hidden ${hiddenButton ? 'todo-hidden-show' : ''} ${selectedTheme === 'purple' ? 'purple-hiddenButton' : selectedTheme === 'veges' ? 'veges-hiddenButton' : selectedTheme === 'zebra' ? 'zebra-hiddenButton' : selectedTheme === 'spider' ? 'spider-hiddenButton' : ''}`} onClick={hideCompletedTasks} >Hide completed tasks</button>
+                    <button className={`todo-hidden ${hiddenButton ? 'todo-hidden-show' : ''} ${selectedTheme === 'purple' ? 'purple-hiddenButton' : selectedTheme === 'veges' ? 'veges-hiddenButton' : selectedTheme === 'zebra' ? 'zebra-hiddenButton' : selectedTheme === 'spider' ? 'spider-hiddenButton' : ''}`} onClick={showCompletedTasks} >Show completed tasks</button>
 
                     {/* Button to add one task in the list */}
                     <button className='todo-add-button' onClick={AddToDo}>
@@ -206,6 +222,46 @@ function App() {
                     </button>
 
                 </div>
+
+                {/* Renders the todo list */}
+                <div className={`todo-body ${selectedTheme === 'purple' ? 'purple-body' : selectedTheme === 'veges' ? 'veges-body' : selectedTheme === 'zebra' ? 'zebra-body' : selectedTheme === 'spider' ? 'spider-body' : ''}`}>
+                    {showInitialText &&
+                        <p className={`initial-todo-text ${selectedTheme === 'purple' ? 'purple-todo-text' : selectedTheme === 'veges' ? 'veges-todo-text' : selectedTheme === 'zebra' ? 'zebra-todo-text' : selectedTheme === 'spider' ? 'spider-todo-text' : ''}`}>
+                            Enlist your tasks here!
+                            <span className={`tooltiptext ${selectedTheme === 'purple' ? 'purple-tooltiptext' : selectedTheme === 'veges' ? 'veges-tooltiptext' : selectedTheme === 'zebra' ? 'zebra-tooltiptext' : selectedTheme === 'spider' ? 'spider-tooltiptext' : ''}`}>Please click the plus button</span>
+                        </p>
+                    }
+                    <ul className='todo-list'>
+                        {todo.map((todo, index) => (
+                            <li key={index} className={`todo-task`}>
+                                <input
+                                    type='checkbox'
+                                    checked={todo.completed && todo.text.trim() !== ''}
+                                    className={`todo-checkbox ${todo.completed && todo.text.length && hideTask ? 'todo-hide-task' : ''} ${todo.completed && todo.text.length && tickDone ? 'todo-ticked' : ''}`}
+                                    onChange={() => handleCheckboxChange(index)}>
+                                </input>
+                                <input
+                                    autoFocus
+                                    type='text'
+                                    placeholder='Enter your task'
+                                    className={`
+                                    todo-input  ${todo.completed && todo.text.length && hideTask ? 'todo-hide-task' : ''}
+                                    ${selectedTheme === 'purple' ? 'purple-todo-input' : selectedTheme === 'veges' ? 'veges-todo-input' : selectedTheme === 'zebra' ? 'zebra-todo-input' : selectedTheme === 'spider' ? 'spider-todo-input' : ''} ${(!todo.text.length && addInputClass) ? 'input-error' : ''}   ${todo.completed && todo.text.length && (selectedTheme === 'purple' ? 'purple-highlight' : selectedTheme === 'veges' ? 'veges-highlight' : selectedTheme === 'zebra' ? 'zebra-highlight' : selectedTheme === 'spider' ? 'spider-highlight' : '')} `}
+                                    value={todo.text}
+                                    onChange={(event) => handleInputChange(index, event)} onKeyDown={handleKeyPress}
+                                    required={true}
+                                    style={{ textDecoration: (todo.completed && todo.text.length) ? 'line-through grey' : '', color: todo.completed ? 'grey' : '' }}>
+                                </input>
+                                <div style={{ display: 'none' }}
+                                    className={`${(!todo.text.length && addInputClass) ? 'input-empty' : ''} `}>
+                                    Please fill in this task
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+               
 
 
 
